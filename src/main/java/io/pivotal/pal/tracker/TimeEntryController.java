df6a9ab8b04c4ea35697468917694eb9b50fdf0a
miyohide <miyohide@gmail.com>
2020-01-28 13:09:44 +0900

    add generics type

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -18,37 +18,37 @@ public class TimeEntryController {
     @PostMapping
     public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntryToCreate) {
         var timeEntry = timeEntryRepository.create(timeEntryToCreate);
-        return new ResponseEntity(timeEntry, HttpStatus.CREATED);
+        return new ResponseEntity<>(timeEntry, HttpStatus.CREATED);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<TimeEntry> read(@PathVariable long id) {
         var timeEntry = timeEntryRepository.find(id);
         if (timeEntry == null) {
-            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         } else {
-            return new ResponseEntity(timeEntry, HttpStatus.OK);
+            return new ResponseEntity<>(timeEntry, HttpStatus.OK);
         }
     }
 
     @GetMapping
     public ResponseEntity<List<TimeEntry>> list() {
-        return new ResponseEntity(timeEntryRepository.list(), HttpStatus.OK);
+        return new ResponseEntity<>(timeEntryRepository.list(), HttpStatus.OK);
     }
 
     @PutMapping("{timeEntryId}")
     public ResponseEntity<TimeEntry> update(@PathVariable long timeEntryId, @RequestBody TimeEntry expected) {
         var updatedEntry = timeEntryRepository.update(timeEntryId, expected);
         if (updatedEntry == null) {
-            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         } else {
-            return new ResponseEntity(updatedEntry, HttpStatus.OK);
+            return new ResponseEntity<>(updatedEntry, HttpStatus.OK);
         }
     }
 
     @DeleteMapping("{timeEntryId}")
     public ResponseEntity<TimeEntry> delete(@PathVariable long timeEntryId) {
         timeEntryRepository.delete(timeEntryId);
-        return new ResponseEntity(null, HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
     }
 }
