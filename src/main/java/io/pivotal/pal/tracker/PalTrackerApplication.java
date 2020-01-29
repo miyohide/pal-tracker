694638d09d88043165e3163ef781b52530b12382
miyohide <miyohide@gmail.com>
2020-01-29 13:28:13 +0900

    Delete commented code.

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -15,7 +15,6 @@ public class PalTrackerApplication {
 
     @Bean
     TimeEntryRepository timeEntryRepository(DataSource dataSource) {
-        //return new InMemoryTimeEntryRepository();
         return new JdbcTimeEntryRepository(dataSource);
     }
 }
